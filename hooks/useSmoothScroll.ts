ee8ccfe8586b6dd821735b62d1ea739cf9093368
Kawsar37 <kawsarali750@gmail.com>
2026-05-03 15:54:48 +0600

    scroll error fixed

--- hooks/useSmoothScroll.ts
+++ hooks/useSmoothScroll.ts
@@ -11,9 +11,6 @@ export function useSmoothScroll() {
         const lenis = new Lenis({
           duration: 1.2,
           easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-          direction: "vertical",
-          gestureDirection: "vertical",
-          smooth: true,
           mouseMultiplier: 1,
           smoothTouch: false,
           touchMultiplier: 2,
