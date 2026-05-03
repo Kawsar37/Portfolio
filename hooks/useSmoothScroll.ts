54cfd5f0c423c9b71255ec024fc1ee3727ef5bd9
Kawsar37 <kawsarali750@gmail.com>
2026-05-03 16:00:09 +0600

    smooth fixed

--- hooks/useSmoothScroll.ts
+++ hooks/useSmoothScroll.ts
@@ -11,10 +11,8 @@ export function useSmoothScroll() {
         const lenis = new Lenis({
           duration: 1.2,
           easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-          mouseMultiplier: 1,
-          smoothTouch: false,
-          touchMultiplier: 2,
-          infinite: false,
+          lerp: 0.1,
+          smoothWheel: true,
         });
 
         function raf(time: number) {
